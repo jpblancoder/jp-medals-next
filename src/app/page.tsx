ca7bb9e9a2af6a307c96204e1823d68871ef5a92
JP Blanchette <empirecoder@gmail.com>
2025-06-01 21:07:07 -0400

    List styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,27 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main>
-      <h1>Medal Count Mini App</h1>
-      <ul>
-        <li>
-          <Link href="/medals/total">Sorted by Total Medals</Link>
+      <h1 className="text-2xl font-bold mb-4 uppercase">Medal Count Mini App</h1>
+      <ul className="list-disc pl-5">
+        <li className="pb-2">
+          <Link href="/medals/total" className="underline">
+            Sorted by Total Medals
+          </Link>
         </li>
-        <li>
-          <Link href="/medals/gold">Sorted by Gold Medals</Link>
+        <li className="pb-2">
+          <Link href="/medals/gold" className="underline">
+            Sorted by Gold Medals
+          </Link>
         </li>
-        <li>
-          <Link href="/medals/silver">Sorted by Silver Medals</Link>
+        <li className="pb-2">
+          <Link href="/medals/silver" className="underline">
+            Sorted by Silver Medals
+          </Link>
         </li>
-        <li>
-          <Link href="/medals/bronze">Sorted by Bronze Medals</Link>
+        <li className="pb-2">
+          <Link href="/medals/bronze" className="underline">
+            Sorted by Bronze Medals
+          </Link>
         </li>
       </ul>
     </main>
